523f4865551f37b87d85d781b5f5ae60de61d16b
abhishek17 <abhishek17aug1991@gmail.com>
2017-11-25 19:32:44 -0700

    animations
    sprite sheet animations

---
+++ TestProjectCedar/Assets/animateControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animateControl : MonoBehaviour {
+
+	// Use this for initialization
+    Animator animComp;
+	void Start () {
+        animComp = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void spriteClick()
+    {
+        Debug.Log("sprite sheet clicked");
+        animComp.Play("click");
+        
+    }
+}
